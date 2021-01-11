f77c0c72124ac83b9bc12d6ddec6017a4db0ea04
issei_windows <inoue.issei.731@gmail.com>
2021-01-12 01:52:45 +0900

    初回コミット

---
+++ android/app/src/main/kotlin/com/example/riverpod_flutterhooks_changehelloworld_stateprovider/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.riverpod_flutterhooks_changehelloworld_stateprovider
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+
+// ポイント1
+// pubspec.yamlの以下の場所に  flutter_hooks: ^0.15.0、
+// hooks_riverpod: ^0.12.1を追記(インデント注意)
+// dependencies:
+//   flutter:
+//     sdk: flutter
+//   flutter_hooks: ^0.15.0
+//   hooks_riverpod: ^0.12.1
+
+// ポイント2
+// main.dartに以下の2行を追加してパッケージを使えるようにする
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+void main() {
+  runApp(
+    // ポイント3
+    // MyApp()をProviderScopeで包む
+    ProviderScope(child: MyApp()),
+  );
+}
+
+// ポイント4
+// 引数を以下のようにしてStateProviderオブジェクトを
+// 生成しstringStateProviderに入れる
+// これによってどのクラスからでも
+// stringStateProviderを参照することができて
+// さらにProviderでは実現できなかった値の変更もできる
+final stringStateProvider = 
+  StateProvider((ref) => 'Hello world');
+
+// ポイント5
+// HookWidgetを継承する
+class MyApp extends HookWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    // 動作確認用1
+    print("buildメソッド");
+
+    // ポイント6
+    // useProvider(stringStateProvider)で
+    // オブジェクトを生成してstringstateに入れる
+    // buildメソッドの中に書くこと
+    final stringstate = useProvider(stringStateProvider);
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Change Hello world'),
+        ),
+        body: Center(
+
+          // ポイント7
+          // stringstate.stateで文字列を取り出す
+          child: Text(stringstate.state),
+
+        ),
+        floatingActionButton: FloatingActionButton(
+          
+          // ボタンを押したとき
+          onPressed: () {
+
+            // ポイント8
+            // stringstate.stateに新しい文字列を入れる
+            stringstate.state = "こんにちは世界";
+            // ポイント4のところで
+            // Provider((ref) => 'Hello world');を
+            // 使っていた場合、こんな感じで
+            // Hello worldを更新することができない
+
+            // 動作確認用2
+            print("ボタン押下");
+
+          },
+        ),
+      ),
+    );
+  }
+}
